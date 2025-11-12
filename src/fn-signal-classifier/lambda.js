49a20477881d99a385b1959f75f3509e74083fc6
LarsDec <lars_declercq@live.be>
2025-11-12 11:04:00 +0100

    determineSignal method basic json parsing

--- src/fn-signal-classifier/lambda.js
+++ src/fn-signal-classifier/lambda.js
@@ -1,13 +1,12 @@
 // Recommended Packages for this Lambda
 const { SNSClient, PublishCommand } = require("@aws-sdk/client-sns");
 const AWSXRay = require('aws-xray-sdk-core');
-
+let JsonMessage = {id: "event-uuid", "detail-type": "creature | hazard | anomaly | dark-signal", source: "htf-2025-aquatopia.sonar-generator", detail: {type: "creature | hazard | anomaly", species: "Sea Turtle", location: "reef-2", intensity: 1}}
 // SNS to send messages to
 const snsArn = process.env.SNSArn;
 
 exports.handler = async (event) => {
     console.log(JSON.stringify(event));
-
     // Determine if the message is a dark signal or not
     let isDark;
 
@@ -27,6 +26,8 @@ exports.handler = async (event) => {
 
 function determineSignal(message) {
     // Return the correct signal-type
+    JsonMessage = JSON.parse(message);
+    const signalType = JsonMessage.detail.type;
     return;
 }
 
